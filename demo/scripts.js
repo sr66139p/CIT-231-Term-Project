5a6b71ece4b6fc7ef1860cff19f06328e0a415fb
sr66139p <sr66139p@pace.edu>
2023-12-13 11:31:05 -0500

    improved dark mode and styling

--- demo/scripts.js
+++ demo/scripts.js
@@ -3,11 +3,13 @@ var isDark = false
 function darkMode() {
     if (isDark == false){
         document.querySelector("body").style.backgroundColor = "black"
-        document.querySelector("body").style.color = "lightblue"
+        document.querySelector("main").style.backgroundColor = "#002d72"
+        document.querySelector("main").style.color = "azure"
         isDark = true
     }else{
         document.querySelector("body").style.backgroundColor = "white"
-        document.querySelector("body").style.color = "blue"
+        document.querySelector("main").style.backgroundColor = "azure"
+        document.querySelector("main").style.color = "#002d72"
         isDark = false
     }
 }
