1a0a2ca75ba13dd2964f1e39fdf9c6375c7c7e44
Aaron <aaron1@metschnet.com>
2023-12-18 12:13:31 -0500

    add darkmode

---
+++ static/darkmode.js
@@ -0,0 +1,15 @@
+var isDark = false
+
+function darkMode() {
+    if (isDark == false) {
+        document.querySelector("body").style.backgroundColor = "black"
+        document.querySelector("main").style.backgroundColor = "#002d72"
+        document.querySelector("main").style.color = "azure"
+        isDark = true
+    } else {
+        document.querySelector("body").style.backgroundColor = "white"
+        document.querySelector("main").style.backgroundColor = "azure"
+        document.querySelector("main").style.color = "#002d72"
+        isDark = false
+    }
+}
