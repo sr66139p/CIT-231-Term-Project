a42448252339e22bfae77b9bfaceeecf8b25ff6d
sr66139p <sr66139p@pace.edu>
2023-12-18 22:03:07 -0500

    finalize work

--- demo/scripts.js
+++ demo/scripts.js
@@ -1,6 +1,6 @@
 var isDark = false
 
-function darkMode() {
+function darkMode(){
     if (isDark == false){
         document.querySelector("body").style.backgroundColor = "black"
         document.querySelector("main").style.backgroundColor = "#002d72"
@@ -12,4 +12,8 @@ function darkMode() {
         document.querySelector("main").style.color = "#002d72"
         isDark = false
     }
+}
+
+function reveal(){
+    alert('Prefessor Keeleys | Web Authorship & Digital Media | CIT 231 | Autumn 2023 | 71000.202370');
 }
